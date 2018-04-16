c926b0457a22cb05b64e31ca5d68ba10162d5412
agpenida <37808893+agpenida@users.noreply.github.com>
2018-04-17 04:48:40 +0800

    Add files via upload

---
+++ dbConnect.php
@@ -0,0 +1,13 @@
+<?php
+//Define your host here.
+$HostName = "localhost";
+
+//Define your database name here.
+$DatabaseName = "id5419520_mobile";
+
+//Define your database username here.
+$HostUser = "id5420801_pendaftaran";
+
+//Define your database password here.
+$HostPass = "godohkebus";
+?>
